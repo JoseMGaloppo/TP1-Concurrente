84fab8b9f98ce0d8ac5becb0544779449cc0157c
JoseMaria <josemgaloppo@gmail.com>
2025-04-27 12:25:53 -0300

    feat: Crear clase ListaPedidos Se creo una nueva clase que contiene un ArrayList con 200 objetos Pedido

---
+++ src/main/resources/ListaPedidos.java
@@ -0,0 +1,27 @@
+package main.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaPedidos {
+    private List<Pedido> listaDePedidos;
+
+    public ListaPedidos() {
+        listaDePedidos = new ArrayList<>();
+        // Llenar con 200 pedidos
+        for (int i = 1; i <= 200; i++) {
+            listaDePedidos.add(new Pedido(EstadoPedido.EN_PREPARACION));
+        }
+    }
+
+    // Método sincronizado para tomar un pedido
+    public synchronized Pedido tomarPedido() {
+        if (!listaDePedidos.isEmpty()) {
+            // Saca el primer pedido de la lista
+            return listaDePedidos.remove(0);
+        } else {
+            return null; // No quedan pedidos
+        }
+    }
+
+}

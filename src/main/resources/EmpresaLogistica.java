8ce4b864145ce08a73abfc70b876ea6b3fcb25ad
facu <facundo.avila.027@mi.unc.edu.ar>
2025-04-26 23:51:05 -0300

    Pequeños cambios y a la cama

--- src/main/resources/EmpresaLogistica.java
+++ src/main/resources/EmpresaLogistica.java
@@ -11,16 +11,13 @@ public class EmpresaLogistica {
         casilleros = new Casillero[20][10];
         registrosPedidos = new RegistroPedidos();
     }
-
-    /*Este proceso se encarga de recibir los pedidos de los usuarios. Se
+    
+    // Métodos para el proceso 1
+    /* Este proceso se encarga de recibir los pedidos de los usuarios. Se
     tienen tres hilos que ejecutan este proceso. Cada hilo intenta seleccionar un casillero
     aleatorio en la matriz, verificando que esté disponible. Si el casillero no está vacío, el hilo
     debe buscar otro casillero que sí lo esté. Una vez ocupado el casillero, el mismo se marca
-    como ocupado y se registra el pedido en el registro de pedidos en preparación.*/
-    public void procesarPedido() {
-
-    }
-    // Métodos para el proceso 1
+    como ocupado y se registra el pedido en el registro de pedidos en preparación. */
 
     public Casillero recorrerCasilleros() {
         Random random = new Random();
@@ -38,10 +35,31 @@ public class EmpresaLogistica {
         }
     }
 
-
-
-
     // Métodos para el proceso 2
+    /* Este proceso es ejecutado por dos hilos, y se encarga de despachar
+    los pedidos del listado de pedidos en preparación. Cada hilo toma un pedido aleatorio del
+    registro de pedidos en preparación y realiza una verificación de los datos del pedido y del
+    usuario. Se establece una probabilidad del 85% de que la información sea correcta y un
+    15% de que sea incorrecta. Si la información fue correcta, el casillero vuelve al estado
+    vacío, y el pedido se elimina del registro de pedidos en preparación y se agrega al registro
+    de pedidos en tránsito. De lo contrario, el casillero pasa a estado fuera de servicio y el
+    pedido se marca como fallido, se elimina del registro de pedidos en preparación y se
+    agrega al registro de pedidos fallidos. */
+
     // Métodos para el proceso 3
+    /* Tres hilos se encargan de ejecutar este paso. Cada hilo selecciona un
+    pedido aleatorio del registro de pedidos en tránsito y con una probabilidad del 90%, lo
+    confirma. Si el pedido es confirmado, se elimina del registro de pedidos en tránsito y se
+    agrega al registro de pedidos entregados. Si el pedido no es confirmado, se elimina del
+    registro de pedidos en tránsito y se agrega al registro de pedidos fallidos. */
+
     // Métodos para el proceso 4
+    /* Al finalizar la ejecución, se debe verificar el estado final de los pedidos
+    para asegurar que las operaciones se hayan realizado correctamente. Este proceso
+    selecciona de manera aleatoria un pedido del registro de pedidos entregados, y con una
+    probabilidad del 95%, el pedido es verificado. Si el pedido fue verificado, se debe eliminar
+    del registro de pedidos entregados y se debe insertar en el registro de pedidos
+    verificados. En caso contrario, se elimina del registro de pedidos entregados y se inserta
+    en el registro de pedidos fallidos. Este proceso es ejecutado por dos hilos. */
+
 }
